b5dcc9dc068378c859f3ef84d3a11a981e7916d9
yazawa8 <yaza_ta@yahoo.co.jp>
2024-04-07 09:30:17 +0900

    Lesson 4の内容を学習

---
+++ src/ConditionOperatorSample02.java
@@ -0,0 +1,7 @@
+public class ConditionOperatorSample02 {
+    public static void main (String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println(a > b);
+    }
+}
---
+++ src/IfElseSample.java
@@ -0,0 +1,16 @@
+public class IfElseSample {
+
+    public static void main(String[] args) {
+        int score = 80; // 成績
+
+        // scoreが60以上ならば
+        if (score >= 60) {
+            // 合格です！と表示
+            System.out.println("合格です！");
+        } else {
+            // 赤点です...と表示
+            System.out.println("赤点です...");
+        }
+    }
+
+}
